5dfcaaff19112039ced14c5f81b671f6cfb753e5
Mohit Singh Panwar <26198379+DragoHex@users.noreply.github.com>
2024-11-11 18:53:39 +0530

    fix server certs scripts

--- ssl-certs-scripts/scripts/certs/server.sh
+++ ssl-certs-scripts/scripts/certs/server.sh
@@ -1,4 +1,7 @@
-# Generate server key and CSR
+# Generate server certificate
 certs_dir="../../certs"
 config_dir="../../config/certs"
-openssl req -new -config $config_dir/server.config -keyout $certs_dir/server.key -out $certs_dir/server.csr -batch -nodes
+
+day=${1:-60}
+
+openssl x509 -req -in $certs_dir/server.csr -signkey $certs_dir/server.key -out $certs_dir/server.crt -days $day
